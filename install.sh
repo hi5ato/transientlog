b0150edda84584a26b2d66cafd6836643f1c8318
azlux <azlux@outlook.com>
2016-04-14 21:26:23 +0200

    systemctl reload added

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ chmod a+x /etc/cron.hourly/transientlog
 
 #update-rc.d transientlog start 00 2 3 4 5 . stop 99 0 1 6 . 
 update-rc.d transientlog defaults
+systemctl daemon-reload
 
 service transientlog start
